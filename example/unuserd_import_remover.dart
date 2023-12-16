4da21f68de1d4b2f8d4f12359fc354c11c9ab2ae
kitaya <kkitaya@nzigen.com>
2023-12-16 21:56:44 +0900

    modify: modify directory configuration for creating dart pub package

---
+++ example/unuserd_import_remover.dart
@@ -0,0 +1,19 @@
+import '../lib/src/message_handler.dart';
+import '../lib/src/file_handler.dart';
+
+void main(List<String> args) async {
+  MessageHandler messageHandler;
+  if (args.isNotEmpty) {
+    messageHandler = MessageHandler(path: args[0]);
+  } else {
+    messageHandler = MessageHandler();
+  }
+  // final res = await messageHandler.getAnalyzedMessage();
+  final res = await messageHandler.createUnusedMapList();
+  if (messageHandler.hasRemovingLine(res)) {
+    final fileHandler = FileHandler();
+    fileHandler.removeLinesFromFile(unusedImportMapList: res);
+  } else {
+    print('Your project has no unused package statement. Awesome!!');
+  }
+}
---
+++ test/unused_import_remover_test.dart
@@ -0,0 +1,6 @@
+import 'package:unused_import_remover/unused_import_remover.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {});
+}
